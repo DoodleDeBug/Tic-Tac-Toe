8c2a0eb8c6bf51ed81c0762a907b4f8d3489fe85
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-06-16 20:40:26 +0100

    output tiles

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const gameBoard = (() => {
-  let board = ["x", "o", "x", "x", "o", "o", "x", "x", "o"];
-  return { board };
+  let boardContent = ["x", "o", "x", "x", "o", "o", "x", "x", "o"];
+
+  const board = document.querySelector(".board");
+  const tile = document.createElement("div");
+  tile.classList.add("tile");
+
+  const displayBoard = boardContent.forEach((square) => {
+    const tile = document.createElement("div");
+    tile.classList.add("tile");
+    tile.innerText = square;
+    board.appendChild(tile);
+  });
+
+  return { boardContent, displayBoard };
 })();
 
 const player = (token) => {

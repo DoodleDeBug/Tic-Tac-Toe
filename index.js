988df5718f49c276c89c0099f3d5a727c069f085
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-06-17 13:01:18 +0100

    rewrite gameboard code in modular way

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-const gameBoard = (() => {
-  let boardContent = ["x", "o", "x", "x", "o", "o", "x", "x", "o"];
+// (() => {
+//   // iffe initialize the game
+//   const body = document.querySelector("body");
 
-  const board = document.querySelector(".board");
-  const tile = document.createElement("div");
-  tile.classList.add("tile");
+//   const gameBoard = (() => {
+//     let boardContent = ["x", "o", "x", "x", "o", "o", "x", "x", "o"];
 
-  const displayBoard = boardContent.forEach((square) => {
-    const tile = document.createElement("div");
-    tile.classList.add("tile");
-    tile.innerText = square;
-    board.appendChild(tile);
-  });
+//     const board = document.querySelector(".board");
 
-  return { boardContent, displayBoard };
+//     const displayBoard = boardContent.forEach((square) => {
+//       const tile = document.createElement("div");
+//       tile.classList.add("tile");
+//       tile.innerText = square;
+//       board.appendChild(tile);
+//     });
+
+//     return { boardContent, displayBoard };
+//   })();
+
+//   const player = (name, token) => {
+//     return { name, token };
+//   };
+// })();
+
+(() => {
+  // iffe initialize the game
+
+  const gameBoard = {
+    boardContent: ["x", "o", "x", "x", "o", "o", "x", "x", "o"],
+    init: () => {
+      gameBoard.cacheDom();
+      gameBoard.render();
+    },
+    cacheDom: () => {
+      this.body = document.querySelector("body");
+      this.board = document.querySelector(".board");
+    },
+    bindEvents: () => {
+      //add events
+    },
+    render: () => {
+      gameBoard.boardContent.forEach((square) => {
+        const tile = document.createElement("div");
+        tile.classList.add("tile");
+        tile.innerText = square;
+        this.board.appendChild(tile);
+      });
+    },
+  };
+
+  gameBoard.init();
+
+  // const player = (name, token) => {
+  //   return { name, token };
+  // };
 })();
-
-const player = (token) => {
-  return { token };
-};

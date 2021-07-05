d13bc4cfbb6bfd0ff1ef1b31a1408bda8602ab3b
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-07-05 15:42:47 +0100

    restart works, now need to clean up and organise code better

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const game = (() => {
   const restartBtn = document.querySelector(".restart");
   const display = document.getElementById("display");
   let players = [];
+  let token;
 
   //bindEvents
   form.addEventListener("submit", getPlayers);
@@ -26,7 +27,7 @@ const game = (() => {
 
     players = setPlayers();
     renderMsg(players[0]);
-    gameBoard();
+    token = players[0].getToken();
     clearForm();
     modal.classList.toggle("hidden");
   }
@@ -53,7 +54,7 @@ const game = (() => {
   function restartGame() {
     modal.classList.toggle("hidden");
     display.innerText = "";
-    gameBoard.restart();
+    clearBoard();
     // console.log("restarted game");
   }
 
@@ -64,125 +65,117 @@ const game = (() => {
 
   ///////////////// GAMEBOARD //////////////////
 
-  const gameBoard = () => {
-    // all things that affect the board
+  let boardContent = ["", "", "", "", "", "", "", "", ""];
 
-    let boardContent = ["", "", "", "", "", "", "", "", ""];
-    // let players = setPlayers();
-    let token = players[0].getToken();
+  // cache DOM
+  const tiles = document.querySelectorAll(".tile");
+  const tilesArray = Array.from(tiles);
 
-    // cache DOM
-    const tiles = document.querySelectorAll(".tile");
-    const tilesArray = Array.from(tiles);
+  //bind events
+  tiles.forEach((tile) => tile.addEventListener("click", makeMove));
 
-    //bind events
-    tiles.forEach((tile) => tile.addEventListener("click", makeMove));
+  function render() {
+    tiles.forEach(
+      (tile) => (tile.innerText = boardContent[tilesArray.indexOf(tile)])
+    ); // inner text of tile corresponds to boardcontent
+  }
 
-    function render() {
-      tiles.forEach(
-        (tile) => (tile.innerText = boardContent[tilesArray.indexOf(tile)])
-      ); // inner text of tile corresponds to boardcontent
+  function makeMove(e) {
+    if (isAvailable(e)) {
+      if (token == players[0].getToken()) {
+        boardContent[isAvailable(e)] = token;
+        render();
+        token = players[1].getToken();
+
+        renderMsg(players[1]);
+      } else if (token == players[1].getToken()) {
+        boardContent[isAvailable(e)] = token;
+        render();
+        token = players[0].getToken();
+
+        renderMsg(players[0]);
+      }
+    } else {
+      alert("Invalid move! Try again");
     }
 
-    function makeMove(e) {
-      if (isAvailable(e)) {
-        if (token == players[0].getToken()) {
-          boardContent[isAvailable(e)] = token;
-          render();
-          token = players[1].getToken();
+    checkWin(boardContent, token);
+  }
 
-          renderMsg(players[1]);
-        } else if (token == players[1].getToken()) {
-          boardContent[isAvailable(e)] = token;
-          render();
-          token = players[0].getToken();
+  function isEqual(...args) {
+    let obj = args[0];
 
-          renderMsg(players[0]);
-        }
-      } else {
-        alert("Invalid move! Try again");
+    for (let i = 1; i < args.length; i++) {
+      if (args[i] == "") {
+        return false; // dont declare win for empty board/ multiple "" in a row
       }
 
-      checkWin(boardContent, token);
-    }
-
-    function isEqual(...args) {
-      let obj = args[0];
-
-      for (let i = 1; i < args.length; i++) {
-        if (args[i] == "") {
-          return false; // dont declare win for empty board/ multiple "" in a row
-        }
-
-        if (obj == args[i]) {
-          continue;
-        } else {
-          return false;
-        }
-      }
-
-      return true;
-    }
-
-    function checkWin(bC, token) {
-      let win =
-        isEqual(bC[0], bC[1], bC[2]) ||
-        isEqual(bC[3], bC[4], bC[5]) ||
-        isEqual(bC[6], bC[7], bC[8]) ||
-        isEqual(bC[0], bC[3], bC[6]) ||
-        isEqual(bC[1], bC[4], bC[7]) ||
-        isEqual(bC[2], bC[5], bC[8]) ||
-        isEqual(bC[0], bC[4], bC[8]) ||
-        isEqual(bC[2], bC[4], bC[6]);
-
-      if (win) {
-        gameOver();
-
-        if (token == "X") {
-          alert(
-            `Player ${players[1].getName()} won! Press the restart button to play again`
-          );
-        } else {
-          alert(
-            `Player ${players[0].getName()} won! Press the restart button to play again`
-          );
-        }
-      } else {
-        checkTie(bC);
-      }
-    }
-
-    function checkTie(bC) {
-      let validMovesLeft = bC.includes("");
-
-      if (!validMovesLeft) {
-        gameOver();
-        alert("It was a tie! Press the restart button to play again");
-      }
-    }
-
-    function gameOver() {
-      tiles.forEach((tile) => tile.removeEventListener("click", makeMove));
-      renderMsg("Game Over");
-    }
-
-    function isAvailable(e) {
-      if (e.target.innerText == "") {
-        return e.target.classList[1];
+      if (obj == args[i]) {
+        continue;
       } else {
         return false;
       }
     }
 
-    function restart() {
-      boardContent = ["", "", "", "", "", "", "", "", ""];
-      render();
-      tiles.forEach((tile) => tile.addEventListener("click", makeMove));
-      token = players[0].getToken();
-    }
+    return true;
+  }
 
-    return {
-      restart,
-    };
-  };
+  function checkWin(bC, token) {
+    let win =
+      isEqual(bC[0], bC[1], bC[2]) ||
+      isEqual(bC[3], bC[4], bC[5]) ||
+      isEqual(bC[6], bC[7], bC[8]) ||
+      isEqual(bC[0], bC[3], bC[6]) ||
+      isEqual(bC[1], bC[4], bC[7]) ||
+      isEqual(bC[2], bC[5], bC[8]) ||
+      isEqual(bC[0], bC[4], bC[8]) ||
+      isEqual(bC[2], bC[4], bC[6]);
+
+    let winner;
+
+    if (win) {
+      if (token == "X") {
+        winner = players[1].getName();
+      } else {
+        winner = players[0].getName();
+      }
+
+      gameOver(winner);
+    } else {
+      checkTie(bC);
+    }
+  }
+
+  function checkTie(bC) {
+    let validMovesLeft = bC.includes("");
+
+    if (!validMovesLeft) {
+      gameOver("tie");
+    }
+  }
+
+  function gameOver(winner) {
+    tiles.forEach((tile) => tile.removeEventListener("click", makeMove));
+    renderMsg("Game Over");
+    if (winner == "tie") {
+      alert("It was a tie! Press the restart button to play again");
+    } else {
+      alert(`Player ${winner} won! Press the restart button to play again`);
+    }
+  }
+
+  function isAvailable(e) {
+    if (e.target.innerText == "") {
+      return e.target.classList[1];
+    } else {
+      return false;
+    }
+  }
+
+  function clearBoard() {
+    boardContent = ["", "", "", "", "", "", "", "", ""];
+    render();
+    tiles.forEach((tile) => tile.addEventListener("click", makeMove));
+    token = players[0].getToken();
+  }
 })();

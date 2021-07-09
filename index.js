d967c2d4a2e7b458e8b49f50fb000631f2caff64
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-07-09 19:22:27 +0100

    ai is less buggy

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const modal = document.querySelector(".modal-backdrop");
 const form = document.querySelector("#form");
 const easy = document.getElementById("easy");
 const hard = document.getElementById("hard");
-const restartBtn = document.querySelector(".restart");
 const display = document.getElementById("display");
 
 const tiles = document.querySelectorAll(".tile"); ///////////////////////////// gameboard
@@ -21,28 +20,11 @@ let boardContent = ["", "", "", "", "", "", "", "", ""]; ///////////////////////
 let gameOption;
 
 //bindEvents
-restartBtn.addEventListener("click", restartGame);
 form.addEventListener("submit", whichGame);
 easy.addEventListener("click", whichGame);
 hard.addEventListener("click", whichGame);
 
 ///////////////// FUNCTIONS //////////////////
-
-function restartGame() {
-  //////////////////////////// global
-  modal.classList.toggle("hidden");
-  display.innerText = "";
-  gameBoard().clearBoard();
-  game.removeTileEventListener();
-  aiGame.removeTileEventListener();
-  tiles.forEach((tile) => {
-    if (Array.from(tile.classList).includes("bg-danger")) {
-      gameBoard().removeHighlight(tile);
-    }
-  });
-
-  gameOption = "";
-}
 
 function whichGame(e) {
   ///////////////////////////// global
@@ -77,14 +59,12 @@ function isEqual(args) {
     if (args[i] == "") {
       return false; // dont declare win for empty board/ multiple "" in a row
     }
-
     if (obj == args[i]) {
       continue;
     } else {
       return false;
     }
   }
-
   return true;
 }
 
@@ -96,7 +76,6 @@ const game = (() => {
   let token;
 
   function start() {
-    ///////////////////////////// game
     players = setPlayers();
     token = players[1].getToken();
     displayTurn(players[0]);
@@ -114,7 +93,6 @@ const game = (() => {
   }
 
   function setPlayers() {
-    ///////////////////////////// game
     let player1 = player(document.querySelector("#p1-name").value, "X");
     let player2 = player(document.querySelector("#p2-name").value, "O");
 
@@ -147,16 +125,20 @@ const game = (() => {
     ///////////////////////////// game
     let position = e.target.classList[1];
 
-    if (gameBoard(players).isAvailable(e)) {
+    if (gameBoard.isAvailable(e)) {
       switchPlayer();
-      gameBoard(players).move(position, token);
-      gameBoard(players).checkWin(boardContent, token);
+      gameBoard.move(position, token);
+      gameBoard.checkWin(boardContent, token);
+      if (gameBoard.checkTie(boardContent) == true) {
+        gameBoard.gameOver("tie");
+      }
     } else {
       alert("Invalid move! Try again");
     }
   }
 
   function assessWinner() {
+    // returns winners token or name
     return token == players[0].getToken()
       ? players[0].getName() == ""
         ? token
@@ -174,42 +156,52 @@ const game = (() => {
 })();
 
 ///////////////// GAMEBOARD //////////////////
-const gameBoard = (players) => {
+const gameBoard = (() => {
+  //cache DOM
+  const restartBtn = document.querySelector(".restart");
+
+  //add event listener
+  restartBtn.addEventListener("click", restartGame);
+
   //variables
-  // let boardContent = ["", "", "", "", "", "", "", "", ""]; ///////////////////////////// gameboard
   const tilesArray = Array.from(tiles);
 
-  // cache DOM
-  // const tiles = document.querySelectorAll(".tile"); ///////////////////////////// gameboard
-
   function render() {
-    ///////////////////////////// gameboard
     tiles.forEach(
       (tile) => (tile.innerText = boardContent[tilesArray.indexOf(tile)])
     );
   }
 
   function isAvailable(e) {
-    ///////////////////////////// gameboard
     return e.target.innerText == "" ? true : false;
   }
 
   function isAvailableComputer(position) {
-    ///////////////////////////// gameboard
     return tilesArray[position].innerText == "" ? true : false;
   }
 
   function move(position, token) {
-    ///////////////////////////// gameboard
-
     boardContent[position] = token;
     render();
+
     // checkWin(boardContent, token);
   }
 
-  function checkWin(bC, token) {
-    ///////////////////////////// gameboard
+  function someoneWon(bC) {
+    return [bC[0], bC[1], bC[2]] || [bC[3], bC[4], bC[5]] || [
+        bC[6],
+        bC[7],
+        bC[8],
+      ] || [bC[0], bC[3], bC[6]] || [bC[1], bC[4], bC[7]] || [
+        bC[2],
+        bC[5],
+        bC[8],
+      ] || [bC[0], bC[4], bC[8]] || [bC[2], bC[4], bC[6]]
+      ? true
+      : false;
+  }
 
+  function checkWin(bC, token) {
     let winOptions = [
       [bC[0], bC[1], bC[2]],
       [bC[3], bC[4], bC[5]],
@@ -231,7 +223,6 @@ const gameBoard = (players) => {
           console.log(winner);
         } else if (gameOption == "easy") {
           token == "X" ? (winner = "player") : (winner = "computer");
-          console.log("AI game");
         } else if (gameOption == "hard") {
           // some code
         }
@@ -242,10 +233,6 @@ const gameBoard = (players) => {
         highlightWin(winningCombo);
       }
     });
-
-    if (!winner) {
-      checkTie(bC);
-    }
   }
 
   function highlightWin(code) {
@@ -271,20 +258,15 @@ const gameBoard = (players) => {
   }
 
   function checkTie(bC) {
-    ///////////////////////////// gameboard
     let validMovesLeft = bC.includes("");
-
-    if (!validMovesLeft) {
-      gameOver("tie");
-      return;
-    }
+    return validMovesLeft ? false : true;
   }
 
   function gameOver(winner) {
-    ///////////////////////////// gameboard
     game.removeTileEventListener();
     aiGame.removeTileEventListener();
     renderMsg("Game Over");
+
     if (winner == "tie") {
       alert("It was a tie! Press the restart button to play again");
       renderMsg("It was a tie!");
@@ -301,20 +283,37 @@ const gameBoard = (players) => {
   }
 
   function clearBoard() {
-    ///////////////////////////// gameboard
     boardContent = ["", "", "", "", "", "", "", "", ""];
     render();
   }
 
+  function restartGame() {
+    //////////////////////////// global
+    modal.classList.toggle("hidden");
+    display.innerText = "";
+    clearBoard();
+    game.removeTileEventListener();
+    aiGame.removeTileEventListener();
+    tiles.forEach((tile) => {
+      if (Array.from(tile.classList).includes("bg-danger")) {
+        removeHighlight(tile);
+      }
+    });
+
+    gameOption = "";
+  }
+
   return {
-    clearBoard,
     isAvailable,
     isAvailableComputer,
     move,
     removeHighlight,
     checkWin,
+    checkTie,
+    someoneWon,
+    gameOver,
   };
-};
+})();
 
 ///////////////// AI //////////////////
 
@@ -324,7 +323,7 @@ const aiGame = (() => {
 
   function start() {
     token = "X";
-    displayTurn();
+    display.innerText = "Your Token is X";
     addTileEventListener();
   }
 
@@ -342,11 +341,20 @@ const aiGame = (() => {
     ///////////////////////////// game
     let position = e.target.classList[1];
 
-    if (gameBoard().isAvailable(e)) {
-      gameBoard().move(position, token);
-      switchToken();
-      displayTurn();
-      computerMove();
+    if (gameBoard.isAvailable(e)) {
+      gameBoard.move(position, token);
+      if (gameBoard.someoneWon == true) {
+        console.log("there is winner");
+        gameBoard.checkWin(boardContent, token);
+      } else if (gameBoard.someoneWon == false) {
+        console.log("there is no winner");
+        if (gameBoard.checkTie(boardContent) == true) {
+          gameBoard.gameOver("tie");
+        }
+      } else {
+        switchToken();
+        computerMove();
+      }
     } else {
       alert("Invalid move! Try again");
     }
@@ -355,31 +363,22 @@ const aiGame = (() => {
   function computerMove() {
     let position = Math.floor(Math.random() * 9);
 
-    if (gameBoard().isAvailableComputer(position)) {
-      gameBoard().move(position, token);
+    if (gameBoard.isAvailableComputer(position)) {
+      gameBoard.move(position, token);
       switchToken();
-      displayTurn();
-      gameBoard().checkWin(boardContent, token);
+      gameBoard.checkWin(boardContent, token);
+      if (gameBoard.checkTie(boardContent) == true) {
+        gameBoard.gameOver("tie");
+      }
     } else {
-      position = Math.floor(Math.random() * 9);
-
-      if (gameBoard().isAvailableComputer(position)) {
-        gameBoard().move(position, token);
-        switchToken();
-        displayTurn();
-        gameBoard().checkWin(boardContent, token);
+      if (gameBoard.checkTie(boardContent) == false) {
+        computerMove();
       }
     }
   }
 
   function switchToken() {
     token == "X" ? (token = "O") : (token = "X");
-  }
-
-  function displayTurn() {
-    token == "X"
-      ? renderMsg(`It's your turn. Your token is ${token}`)
-      : renderMsg(`It's the Computers turn.`);
   }
 
   return {
